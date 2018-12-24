52bee1339b336b268b24db4b99eeb69cb209634c
kazu7391 <vinhtq.0703@gmail.com>
2018-12-24 19:39:20 +0700

    Modules of Framework

---
+++ Modules/Diva Ajax Search/catalog/language/en-gb/diva/module/dvsearch.php
@@ -0,0 +1,5 @@
+<?php
+// Text
+$_['text_search']       = 'Search';
+$_['text_category']     = 'All Categories';
+$_['text_empty']        = 'There are no products to list in this category.';
---
+++ Modules/Diva Blog v1.1/admin/language/en-gb/diva/blog/list.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']          = '<b style="color: #eb5202;"><i>Blog Categories</i></b>';
+$_['page_title']             = 'Blog Categories';
+
+// Text
+$_['text_success']           = 'Success: You have modified Blog Categories';
+$_['text_list']              = 'Blog Categories';
+$_['text_add']               = 'Add Category';
+$_['text_edit']              = 'Edit Category';
+$_['text_extension']         = 'Extensions';
+$_['text_blog']              = 'Blog';
+
+// Column
+$_['column_name']            = 'Name';
+$_['column_status']          = 'Status';
+$_['column_action']          = 'Action';
+
+// Entry
+$_['entry_name']             = 'Name';
+$_['entry_post_list']        = 'Posts';
+$_['entry_status']           = 'Status';
+
+// Error
+$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']       = 'Warning: You do not have permission to modify Blog Categories!';
+$_['error_name']             = 'Article Name must be greater than 3 and less than 255 characters!';
+$_['error_meta_title']       = 'Meta Title must be greater than 3 and less than 255 characters!';
+$_['error_keyword']          = 'SEO keyword already in use!';
