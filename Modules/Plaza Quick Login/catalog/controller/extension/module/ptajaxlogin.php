f5f98a9bc5906f01e1ea03a4cf3d89499519b004
kazu7391 <vinhtq.0703@gmail.com>
2018-12-25 19:08:32 +0700

    Quick Login

---
+++ Modules/Plaza Quick Login/catalog/controller/extension/module/ptajaxlogin.php
@@ -0,0 +1,51 @@
+<?php
+class ControllerExtensionModulePtajaxlogin extends Controller
+{
+    public function index() {
+        $this->load->language('plaza/module/ptajaxlogin');
+
+        $enable_status = $this->config->get('module_ptajaxlogin_status');
+        if($enable_status == '1') {
+            $data['status'] = true;
+        } else {
+            $data['status'] = false;
+        }
+
+        $enable_redirect = $this->config->get('module_ptajaxlogin_redirect_status');
+        if($enable_redirect == '1') {
+            $data['redirect'] = true;
+        } else {
+            $data['redirect'] = false;
+        }
+
+        $store_id = $this->config->get('config_store_id');
+
+        if (!empty($_SERVER['HTTPS'])) {
+            // SSL connection
+            $common_url = str_replace('http://', 'https://', $this->config->get('config_url'));
+        } else {
+            $common_url = $this->config->get('config_url');
+        }
+
+        if(isset($this->config->get('module_ptcontrolpanel_loader_img')[$store_id])) {
+            $data['loader_img'] = $common_url . 'image/' . $this->config->get('module_ptcontrolpanel_loader_img')[$store_id];
+        } else {
+            $data['loader_img'] = $common_url . 'image/plaza/ajax-loader.gif';;
+        }
+
+        if (file_exists(DIR_TEMPLATE . $this->config->get($this->config->get('config_theme') . '_directory') . '/stylesheet/plaza/ajaxlogin/ajaxlogin.css')) {
+            $this->document->addStyle('catalog/view/theme/'.$this->config->get($this->config->get('config_theme') . '_directory').'/stylesheet/plaza/ajaxlogin/ajaxlogin.css');
+        } else {
+            $this->document->addStyle('catalog/view/theme/default/stylesheet/plaza/ajaxlogin/ajaxlogin.css');
+        }
+
+        $this->document->addScript('catalog/view/javascript/plaza/ajaxlogin/ajaxlogin.js');
+
+        $data['ajax_login_content'] = $this->load->controller('plaza/login');
+        $data['ajax_register_content'] = $this->load->controller('plaza/register');
+        $data['ajax_success_content'] = $this->load->controller('plaza/register/success');
+        $data['ajax_logoutsuccess_content'] = $this->load->controller('plaza/login/logoutSuccess');
+
+        return $this->load->view('plaza/module/ptajaxlogin', $data);
+    }
+}
