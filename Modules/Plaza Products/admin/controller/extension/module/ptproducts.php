a22db7544247fcdb16bbc56cfe0651449f3d14f7
kazu7391 <vinhtq.0703@gmail.com>
2019-01-03 15:14:22 +0700

    Product module

---
+++ Modules/Plaza Products/admin/controller/extension/module/ptproducts.php
@@ -0,0 +1,397 @@
+<?php
+class ControllerExtensionModulePtproducts extends Controller {
+    private $error = array();
+
+    public function index() {
+
+        $this->load->language('extension/module/ptproducts');
+
+        $this->document->setTitle($this->language->get('page_title'));
+
+        $this->load->model('setting/module');
+
+        $this->load->model('tool/image');
+
+        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+            if (!isset($this->request->get['module_id'])) {
+                $this->model_setting_module->addModule('ptproducts', $this->request->post);
+            } else {
+                $this->model_setting_module->editModule($this->request->get['module_id'], $this->request->post);
+            }
+
+            $this->session->data['success'] = $this->language->get('text_success');
+
+            $this->response->redirect($this->url->link('plaza/module', 'user_token=' . $this->session->data['user_token']));
+        }
+
+        $this->load->model('localisation/language');
+
+        $data['languages'] = array();
+
+        $languages = $this->model_localisation_language->getLanguages();
+
+        foreach ($languages as $language){
+            if ($language['status']) {
+                $data['languages'][] = array(
+                    'name'  => $language['name'],
+                    'language_id' => $language['language_id'],
+                    'code' => $language['code']
+                );
+            }
+        }
+
+        if (isset($this->error['warning'])) {
+            $data['error_warning'] = $this->error['warning'];
+        } else {
+            $data['error_warning'] = '';
+        }
+
+        if (isset($this->error['name'])) {
+            $data['error_name'] = $this->error['name'];
+        } else {
+            $data['error_name'] = '';
+        }
+
+        if (isset($this->error['width'])) {
+            $data['error_width'] = $this->error['width'];
+        } else {
+            $data['error_width'] = '';
+        }
+
+        if (isset($this->error['height'])) {
+            $data['error_height'] = $this->error['height'];
+        } else {
+            $data['error_height'] = '';
+        }
+
+        $data['breadcrumbs'] = array();
+
+        $data['breadcrumbs'][] = array(
+            'text' => $this->language->get('text_home'),
+            'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+        );
+
+        $data['breadcrumbs'][] = array(
+            'text' => $this->language->get('text_module'),
+            'href' => $this->url->link('plaza/module', 'user_token=' . $this->session->data['user_token'])
+        );
+
+        if (!isset($this->request->get['module_id'])) {
+            $data['breadcrumbs'][] = array(
+                'text' => $this->language->get('heading_title'),
+                'href' => $this->url->link('extension/module/ptproducts', 'user_token=' . $this->session->data['user_token'], true)
+            );
+        } else {
+            $data['breadcrumbs'][] = array(
+                'text' => $this->language->get('heading_title'),
+                'href' => $this->url->link('extension/module/ptproducts', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], true)
+            );
+        }
+
+        if (!isset($this->request->get['module_id'])) {
+            $data['action'] = $this->url->link('extension/module/ptproducts', 'user_token=' . $this->session->data['user_token'], true);
+        } else {
+            $data['action'] = $this->url->link('extension/module/ptproducts', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], true);
+        }
+
+        $data['cancel'] = $this->url->link('plaza/module', 'user_token=' . $this->session->data['user_token']);
+
+        if (isset($this->request->get['module_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
+            $module_info = $this->model_setting_module->getModule($this->request->get['module_id']);
+        }
+
+        $data['user_token'] = $this->session->data['user_token'];
+
+        if (isset($this->request->post['status'])) {
+            $data['status'] = $this->request->post['status'];
+        } elseif (!empty($module_info)) {
+            $data['status'] = $module_info['status'];
+        } else {
+            $data['status'] = 1;
+        }
+
+        if (isset($this->request->post['name'])) {
+            $data['name'] = $this->request->post['name'];
+        } elseif (!empty($module_info)) {
+            $data['name'] = $module_info['name'];
+        } else {
+            $data['name'] = '';
+        }
+
+        if (isset($this->request->post['module_title'])) {
+            $data['module_title'] = $this->request->post['module_title'];
+        } elseif (!empty($module_info)) {
+            $data['module_title'] = $module_info['module_title'];
+        } else {
+            $data['module_title'] = array();
+        }
+
+        if (isset($this->request->post['show_module_description'])) {
+            $data['show_module_description'] = $this->request->post['show_module_description'];
+        } elseif (!empty($module_info)) {
+            $data['show_module_description'] = $module_info['show_module_description'];
+        } else {
+            $data['show_module_description'] = '';
+        }
+
+        if (isset($this->request->post['module_description'])) {
+            $data['module_description'] = $this->request->post['module_description'];
+        } elseif (!empty($module_info)) {
+            $data['module_description'] = $module_info['module_description'];
+        } else {
+            $data['module_description'] = array();
+        }
+
+        if (isset($this->request->post['module_type'])) {
+            $data['module_type'] = $this->request->post['module_type'];
+        } elseif (!empty($module_info)) {
+            $data['module_type'] = $module_info['module_type'];
+        } else {
+            $data['module_type'] = 'single_tab';
+        }
+
+        $this->load->model('catalog/product');
+
+        $this->document->addStyle('view/stylesheet/plaza/themeadmin.css');
+        $this->document->addScript('view/javascript/plaza/switch-toggle/js/bootstrap-toggle.min.js');
+        $this->document->addStyle('view/javascript/plaza/switch-toggle/css/bootstrap-toggle.min.css');
+
+        $data['header'] = $this->load->controller('common/header');
+        $data['column_left'] = $this->load->controller('common/column_left');
+        $data['footer'] = $this->load->controller('common/footer');
+
+        $this->response->setOutput($this->load->view('plaza/module/ptproducts', $data));
+    }
+
+    public function autocomplete() {
+        $json = array();
+
+        if (isset($this->request->get['filter_name']) || isset($this->request->get['filter_model'])) {
+            $this->load->model('catalog/product');
+            $this->load->model('catalog/option');
+
+            if (isset($this->request->get['filter_name'])) {
+                $filter_name = $this->request->get['filter_name'];
+            } else {
+                $filter_name = '';
+            }
+
+            if (isset($this->request->get['filter_model'])) {
+                $filter_model = $this->request->get['filter_model'];
+            } else {
+                $filter_model = '';
+            }
+
+            if (isset($this->request->get['limit'])) {
+                $limit = $this->request->get['limit'];
+            } else {
+                $limit = 5;
+            }
+
+            $filter_data = array(
+                'filter_name'  => $filter_name,
+                'filter_model' => $filter_model,
+                'start'        => 0,
+                'limit'        => $limit
+            );
+
+            $results = $this->model_catalog_product->getProducts($filter_data);
+
+            foreach ($results as $result) {
+                $option_data = array();
+
+                $product_options = $this->model_catalog_product->getProductOptions($result['product_id']);
+
+                foreach ($product_options as $product_option) {
+                    $option_info = $this->model_catalog_option->getOption($product_option['option_id']);
+                    if ($option_info) {
+                        $product_option_value_data = array();
+                        foreach ($product_option['product_option_value'] as $product_option_value) {
+                            $option_value_info = $this->model_catalog_option->getOptionValue($product_option_value['option_value_id']);
+
+                            if ($option_value_info) {
+                                $product_option_value_data[] = array(
+                                    'product_option_value_id' => $product_option_value['product_option_value_id'],
+                                    'option_value_id'         => $product_option_value['option_value_id'],
+                                    'name'                    => $option_value_info['name'],
+                                    'price'                   => (float)$product_option_value['price'] ? $this->currency->format($product_option_value['price'], $this->config->get('config_currency')) : false,
+                                    'price_prefix'            => $product_option_value['price_prefix'],
+                                );
+                            }
+                        }
+
+                        $option_data[] = array(
+                            'product_option_id'    => $product_option['product_option_id'],
+                            'product_option_value' => $product_option_value_data,
+                            'option_id'            => $product_option['option_id'],
+                            'name'                 => $option_info['name'],
+                            'type'                 => $option_info['type'],
+                            'value'                => $product_option['value'],
+                            'required'             => $product_option['required']
+                        );
+                    }
+                }
+                $this->load->model('tool/image');
+                $image = $this->model_tool_image->resize($result['image'],40,40);
+                $json[] = array(
+                    'product_id' => $result['product_id'],
+                    'name'       => strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8')),
+                    'model'      => $result['model'],
+                    'option'     => $option_data,
+                    'price'      => $result['price'],
+                    'image'      => $image,
+                );
+            }
+        }
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+
+    public function getProductCategory() {
+        $json = array();
+        if (isset($this->request->get['filter_name']) || isset($this->request->get['filter_model'])) {
+            $this->load->model('catalog/product');
+            $this->load->model('catalog/option');
+
+            if (isset($this->request->get['filter_name'])) {
+                $filter_name = $this->request->get['filter_name'];
+            } else {
+                $filter_name = '';
+            }
+
+            if (isset($this->request->get['category_id'])) {
+                $category_id = $this->request->get['category_id'];
+            } else {
+                $category_id = '';
+            }
+
+            if (isset($this->request->get['filter_model'])) {
+                $filter_model = $this->request->get['filter_model'];
+            } else {
+                $filter_model = '';
+            }
+
+            if (isset($this->request->get['limit'])) {
+                $limit = $this->request->get['limit'];
+            } else {
+                $limit = 5;
+            }
+
+            $filter_data = array(
+                'filter_category_id' => $category_id,
+                'filter_name'  => $filter_name,
+                'filter_model' => $filter_model,
+                'start'        => 0,
+                'limit'        => $limit
+            );
+
+            $results = $this->model_catalog_product->getProducts($filter_data);
+
+            foreach ($results as $result) {
+                $option_data = array();
+
+                $product_options = $this->model_catalog_product->getProductOptions($result['product_id']);
+
+                foreach ($product_options as $product_option) {
+                    $option_info = $this->model_catalog_option->getOption($product_option['option_id']);
+                    if ($option_info) {
+                        $product_option_value_data = array();
+                        foreach ($product_option['product_option_value'] as $product_option_value) {
+                            $option_value_info = $this->model_catalog_option->getOptionValue($product_option_value['option_value_id']);
+
+                            if ($option_value_info) {
+                                $product_option_value_data[] = array(
+                                    'product_option_value_id' => $product_option_value['product_option_value_id'],
+                                    'option_value_id'         => $product_option_value['option_value_id'],
+                                    'name'                    => $option_value_info['name'],
+                                    'price'                   => (float)$product_option_value['price'] ? $this->currency->format($product_option_value['price'], $this->config->get('config_currency')) : false,
+                                    'price_prefix'            => $product_option_value['price_prefix'],
+                                );
+                            }
+                        }
+
+                        $option_data[] = array(
+                            'product_option_id'    => $product_option['product_option_id'],
+                            'product_option_value' => $product_option_value_data,
+                            'option_id'            => $product_option['option_id'],
+                            'name'                 => $option_info['name'],
+                            'type'                 => $option_info['type'],
+                            'value'                => $product_option['value'],
+                            'required'             => $product_option['required']
+                        );
+                    }
+                }
+                $this->load->model('tool/image');
+                $image = $this->model_tool_image->resize($result['image'],40,40);
+                $json[] = array(
+                    'product_id' => $result['product_id'],
+                    'name'       => strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8')),
+                    'model'      => $result['model'],
+                    'option'     => $option_data,
+                    'price'      => $result['price'],
+                    'image'      => $image,
+                );
+            }
+        }
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+
+    public function autocompleteCategory() {
+        $json = array();
+
+        if (isset($this->request->get['filter_name'])) {
+            $this->load->model('catalog/category');
+
+            $filter_data = array(
+                'filter_name' => $this->request->get['filter_name'],
+                'sort'        => 'name',
+                'order'       => 'ASC',
+                'start'       => 0,
+                'limit'       => 5
+            );
+
+            $results = $this->model_catalog_category->getCategories($filter_data);
+
+            foreach ($results as $result) {
+                $json[] = array(
+                    'category_id' => $result['category_id'],
+                    'name'        => strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8'))
+                );
+            }
+        }
+
+        $sort_order = array();
+
+        foreach ($json as $key => $value) {
+            $sort_order[$key] = $value['name'];
+        }
+
+        array_multisort($sort_order, SORT_ASC, $json);
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+
+    protected function validate() {
+        if (!$this->user->hasPermission('modify', 'extension/module/ptproducts')) {
+            $this->error['warning'] = $this->language->get('error_permission');
+        }
+
+        if ((utf8_strlen($this->request->post['name']) < 3) || (utf8_strlen($this->request->post['name']) > 64)) {
+            $this->error['name'] = $this->language->get('error_name');
+        }
+
+        if (!$this->request->post['width']) {
+            $this->error['width'] = $this->language->get('error_width');
+        }
+
+        if (!$this->request->post['height']) {
+            $this->error['height'] = $this->language->get('error_height');
+        }
+
+        return !$this->error;
+    }
+}
---
+++ Modules/Plaza Products/admin/language/en-gb/extension/module/ptproducts.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']    		 = '<b style="color: #eb5202;"><i><i class="a fa fa-shopping-bag"></i> Plaza Products </i></b>';
+$_['page_title']             = 'Plaza Products';
+
+// Text
+$_['text_module']      		 = 'Modules';
+$_['text_success']     		 = 'Success: You have modified Plaza Products module!';
+$_['text_edit']        		 = 'Edit Plaza Products Module';
+$_['text_type_single']       = 'Single';
+$_['text_type_multiple']     = 'Multiple Tabs';
+
+// Entry
+$_['entry_name']       		 = 'Module Name';
+$_['entry_status']           = 'Status';
+$_['entry_title']            = 'Module Title';
+$_['entry_show_module_des']  = 'Show Module Description';
+$_['entry_module_type']      = 'Module Type';
+
+// Help
+$_['help_product']     = '(Autocomplete)';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Plaza Products module!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_width']      = 'Width required!';
+$_['error_height']     = 'Height required!';
+$_['error_category']     = 'You have to select a category !';
