eaa54c2e00c132b4ef4ae22b17da90b2fd64d529
unknown <ducanh.fcds.gtvt@gmail.com>
2019-01-29 11:00:56 +0700

    debaco

--- Modules/Plaza Products/catalog/language/en-gb/plaza/module/ptproducts.php
+++ Modules/Plaza Products/catalog/language/en-gb/plaza/module/ptproducts.php
@@ -1,9 +1,3 @@
 <?php
 $_['module_product_title'] = "Products";
-$_['text_sale'] = "Sale";
-$_['text_new'] = "New";
 $_['text_empty'] = "There is no product.";
-$_['text_day'] = "days";
-$_['text_hour'] = "hours";
-$_['text_min'] = "minutes";
-$_['text_sec'] = "seconds";
